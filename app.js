06c60e2545cddbb202e5b0165a6ab46e017848e1
TripleOG27 <zhiyanov.detelin@gmail.com>
2020-12-08 13:38:18 +0200

    'imageswitch'

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT ||3000;
 const bodyParser = require('body-parser');
+
+//console.log('all fine till here')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
-console.log('running')
+app.use(express.static(__dirname+'/images'));
+//console.log('running')
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname +'/index.html'));
+    
 });
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
