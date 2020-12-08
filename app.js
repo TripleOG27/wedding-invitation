bc4ef29649b21ae325b67c3910fe82d4f1626457
TripleOG27 <zhiyanov.detelin@gmail.com>
2020-12-08 12:09:48 +0200

    'portfix'

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT ||3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 console.log('running')
 
 app.get('/', function(req, res) {
